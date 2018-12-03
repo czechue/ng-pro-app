1f8b1cd93608ed5aa4aa63688578b27b093fad9d
michal.lester87@gmail.com <michal.lester87@gmail.com>
2018-12-03 23:53:44 +0100

    finished auth components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Store } from 'store';
 
 // feature modules
+import { AuthModule } from '../auth/auth.module';
 
 // containers
 import { AppComponent } from './containers/app/app.component';
@@ -17,7 +18,8 @@ export const ROUTES: Routes = [];
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AuthModule
   ],
   declarations: [
     AppComponent
@@ -30,3 +32,12 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+// var config = {
+//   apiKey: "AIzaSyB_PLtl7HPud0uy2Gb_PP-4N3WX2nX-BWo",
+//   authDomain: "fitness-app-72064.firebaseapp.com",
+//   databaseURL: "https://fitness-app-72064.firebaseio.com",
+//   projectId: "fitness-app-72064",
+//   storageBucket: "fitness-app-72064.appspot.com",
+//   messagingSenderId: "344970180156"
+// };
---
+++ src/auth/register/containers/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+	selector: 'register',
+	template: `
+    <div>
+      <auth-form (submitted)="registerUser($event)">
+        <h1>Register</h1>
+        <a routerLink="/auth/login">Alraedy have an account?</a>
+        <button type="submit">Create account</button>
+      </auth-form>
+    </div>
+  `
+})
+export class RegisterComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+
+	registerUser(event: FormGroup) {
+		console.log(event.value);
+	}
+}
