1f3d10281f255195008f69e7a5671a6f42ed2a77
michal.lester87@gmail.com <michal.lester87@gmail.com>
2018-12-04 23:14:39 +0100

    reactive store and af observables

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Store } from 'store';
+import {
+	AuthService,
+	User
+} from '../../../auth/shared/services/auth/auth.service';
 
 @Component({
 	selector: 'app-root',
 	styleUrls: [ 'app.component.scss' ],
 	template: `
     <div>
-      Hello Ultimate Angular!
+      <h1>{{ user$ | async | json }}</h1>
       <div class="wrapper">
         <router-outlet></router-outlet>
       </div>
@@ -13,5 +21,21 @@ import { Component } from '@angular/core';
   `
 })
 export class AppComponent {
-	constructor() {}
+	user$: Observable<User>;
+	subscription: Subscription;
+
+	constructor(private store: Store, private authService: AuthService) {}
+
+	ngOnInit(): void {
+		//Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+		//Add 'implements OnInit' to the class.
+		this.subscription = this.authService.auth$.subscribe();
+		this.user$ = this.store.select<User>('user');
+	}
+
+	ngOnDestroy(): void {
+		//Called once, before the instance is destroyed.
+		//Add 'implements OnDestroy' to the class.
+		this.subscription.unsubscribe();
+	}
 }
