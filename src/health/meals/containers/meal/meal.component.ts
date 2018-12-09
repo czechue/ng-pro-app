c04182484d82c9cc44c7324285057bdda4f023ce
michal.lester87@gmail.com <michal.lester87@gmail.com>
2018-12-09 12:35:06 +0100

    add meal-form component

--- src/health/meals/containers/meal/meal.component.ts
+++ src/health/meals/containers/meal/meal.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Meal } from '../../../shared/services/meals/meals.service';
 
 @Component({
 	selector: 'meal',
 	template: `
-    <div class="meal">Meal</div>
+		<div class="meal">
+			<div class="meal__title">
+				<h1>
+					<img src="/img/food.svg">
+					<span>Create meal</span>
+				</h1>
+			</div>
+			<div>
+				<meal-form
+					(create)="addMeal($event)">
+				</meal-form>
+			</div>
+		</div>
   `,
 	styleUrls: [ 'meal.component.scss' ]
 })
-export class MealComponent implements OnInit {
+export class MealComponent {
 	constructor() {}
 
-	ngOnInit() {}
+	addMeal(event: Meal) {
+		console.log('meal', event);
+	}
 }
--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
-import { MealsComponent } from './containers/meals/meals.component';
-import { MealComponent } from './containers/meal/meal.component';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+
+import { MealsComponent } from './containers/meals/meals.component';
+import { MealComponent } from './containers/meal/meal.component';
+import { MealFormComponent } from './components/meal-form/meal-form.component';
 
 export const ROUTES: Routes = [
 	{ path: '', component: MealsComponent },
@@ -19,6 +21,6 @@ export const ROUTES: Routes = [
 		RouterModule.forChild(ROUTES),
 		SharedModule
 	],
-	declarations: [ MealsComponent, MealComponent ]
+	declarations: [ MealsComponent, MealComponent, MealFormComponent ]
 })
 export class MealsModule {}
